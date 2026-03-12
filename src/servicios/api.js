50a949798ddfd025f957d4d274f79332e0166dca
LiizethRR <ramirezramirezlizeth900@gmail.com>
2026-03-12 17:42:57 -0600

    Rtutas para Railway

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: "https://backendbaguette02-production-1adf.up.railway.app/api/v1"
 });
 
 export default API;
